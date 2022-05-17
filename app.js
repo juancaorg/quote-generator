7ee6d027931d13594b7869b724ff4c5dce2ca31f
Juan Carrillo <juan@juanca.org>
2022-05-17 11:46:13 -0500

    fix(app): change constants to all caps

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ let apiQuotes = [];
 // Show new quote.
 function newQuote() {
 	// Pick a random quote from API Quotes array.
-	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
-	console.log(quote.text);
+	const QUOTE = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+	console.log(QUOTE.text);
 }
 
 // Get quotes from API.
 async function getQuotes() {
 	const API_URL = 'https://type.fit/api/quotes';
-	const response = await fetch(API_URL);
-	apiQuotes = await response.json();
+	const RESPONSE = await fetch(API_URL);
+	apiQuotes = await RESPONSE.json();
 	newQuote();
 }
 
