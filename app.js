ddbb90ade8150e951a208d6e530ed2b31993c02d
Juan Carrillo <juan@juanca.org>
2022-05-17 12:29:00 -0500

    feat(app): add quote-text and quote-author dynamically to the page and fetching from the API

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+// Get elements by Id and add them to constants for DOM manipulation.
+const QUOTE_CONTAINER = document.getElementById('quote-container');
+const QUOTE_TEXT = document.getElementById('quote');
+const AUTHOR_TEXT = document.getElementById('author');
+const TWITTER_BTN = document.getElementById('twitter');
+const NEW_QUOTE_BTN = document.getElementById('new-quote');
+
+// Global variable array for quotes fetched from the API.
 let apiQuotes = [];
 
 // Show new quote.
 function newQuote() {
 	// Pick a random quote from API Quotes array.
 	const QUOTE = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
-	console.log(QUOTE.text);
+	// Add quote text and author to the page dynamically.
+	AUTHOR_TEXT.textContent = QUOTE.author;
+	QUOTE_TEXT.textContent = QUOTE.text;
 }
 
 // Get quotes from API.
