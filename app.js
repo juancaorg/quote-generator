c8ed902a84e113a013252de1674b69d3f2c35f83
Juan Carrillo <juan@juanca.org>
2022-05-17 12:38:52 -0500

    feat(app): check quote length to determine styling

--- app.js
+++ app.js
@@ -12,12 +12,21 @@ let apiQuotes = [];
 function newQuote() {
 	// Pick a random quote from API Quotes array.
 	const QUOTE = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+	
 	// Check if author field is blank or null and replace it with 'Unkwown'.
 	if(!QUOTE.author) {
 		AUTHOR_TEXT.textContent = 'Unkwown';
 	} else {
 		AUTHOR_TEXT.textContent = QUOTE.author;
 	}
+
+	// Check quote length to determine styling.
+	if (QUOTE.text.length > 50) {
+		QUOTE_TEXT.classList.add('long-quote');
+	} else {
+		QUOTE_TEXT.classList.remove('long-quote');
+	}
+
 	// Add quote text and author to the page dynamically.
 	QUOTE_TEXT.textContent = QUOTE.text;
 }
